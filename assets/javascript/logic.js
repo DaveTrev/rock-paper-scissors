61479aee81402f22dc51af447d358e32f9f48cb6
DaveTrev <trevaskisdavid@gmail.com>
2023-02-27 12:16:41 +0000

    added computer choices and win lose conditions

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -5,8 +5,54 @@ const possibleChoices = document.querySelectorAll('button');
 
 //creating user choice button interaction!
 let userChoice;
+let computerChoice
+let result
 
 possibleChoices.forEach(possibleChoices => possibleChoices.addEventListener('click', (e) => {
     userChoice = e.target.id;
     userChoiceDisplay.innerHTML = userChoice;
+    generateComputerChoice();
+    getResult();
 }));
+
+// creating random choice of the computer
+function generateComputerChoice() {
+const randomNumber = Math.floor(Math.random() * 3) + 1 // to return possible choices for computer (could also use possibleChoices.length)
+
+if (randomNumber === 1) {
+computerChoice = 'Rock'
+}
+if (randomNumber === 2) {
+    computerChoice = 'Scissors'
+    }
+    if (randomNumber === 3) {
+        computerChoice = 'Paper'
+        }
+computerChoiceDisplay.innerHTML = computerChoice;
+}
+
+//win / lose conditions for game
+function getResult() {
+    if (computerChoice === userChoice) {
+        result = 'its a draw!'
+}
+if (computerChoice === 'Rock' && userChoice === 'Paper') {
+    result = 'you win!'
+}
+if (computerChoice === 'Rock' && userChoice === 'Scissors') {
+    result = 'you lose!'
+}
+if (computerChoice === 'Paper' && userChoice === 'Scissors') {
+    result = 'you win!'
+}
+if (computerChoice === 'Paper' && userChoice === 'Rock') {
+    result = 'you lose!'
+}
+if (computerChoice === 'Scissors' && userChoice === 'Rock') {
+    result = 'you win!'
+}
+if (computerChoice === 'Scissors' && userChoice === 'Paper') {
+    result = 'you lose!'
+}
+resultDisplay.innerHTML = result;
+}
